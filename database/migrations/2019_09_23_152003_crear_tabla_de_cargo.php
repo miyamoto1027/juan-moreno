852b7e5cbfa77c427a2f59fbd5ea98ae1a8f1976
juan <manius1027@hotmail.com>
2019-09-23 10:29:07 -0500

    se creo la  tabla  de  cargos

---
+++ database/migrations/2019_09_23_152003_crear_tabla_de_cargo.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CrearTablaDeCargo extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cargos',function(Blueprint $table){
+
+            $table->increments('id');
+            $table->string('nomcargo');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+       Schema::dropIfExists('cargos');
+    }
+}
